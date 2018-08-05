c7f5d159cfdf92a099b51ae6b49d90b10dc6f82f
mansour20-meet <mansour20@meet.mit.edu>
2018-08-05 09:30:15 -0400

    Stopping here

---
+++ day6.py
@@ -0,0 +1,5 @@
+number = 2
+while number in eange(2):
+    print(number)
+
+
--- factorise.py
+++ factorise.py
@@ -1,3 +1,5 @@
-count=int(input('enter a factor prise number'))
+for number in range( 2,150 )
+is_factor = (150 % number ==0)
+
 
 
